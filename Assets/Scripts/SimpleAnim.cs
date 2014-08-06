5695a768dc4a86dea78a06c448370e83e992dff8
mattpbooth <synchingfeeling@gmail.com>
2014-08-06 22:25:38 +0100

    Initial Commit

---
+++ Assets/Scripts/SimpleAnim.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleAnim : MonoBehaviour {
+
+	public Sprite[] _sprites;
+	public float _delayBetweenPlays = 0.0f;
+	public float _speed = 0.33f;
+	 
+	private enum PlayState
+	{
+		Playing,
+		Waiting
+	}
+	private int _currentFrame = 0;
+	private float _lastPlayed = 0.0f;
+	private float _currentPlay = 0.0f;
+	private PlayState _playState = PlayState.Waiting;
+	private SpriteRenderer _spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		_spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (_playState) {
+			case PlayState.Waiting:
+				_lastPlayed += Time.deltaTime;			
+				if (_lastPlayed >= _delayBetweenPlays) {
+					_playState = PlayState.Playing;
+					_lastPlayed = 0.0f;
+				}
+				break;
+
+			case PlayState.Playing:			
+				_currentPlay += Time.deltaTime;
+				if (_currentPlay >= _speed) {
+					++_currentFrame;
+					if(_currentFrame >= _sprites.Length)
+					{
+						_playState = PlayState.Waiting; 
+						_currentFrame = 0;
+					}
+					
+					_spriteRenderer.sprite = _sprites[_currentFrame];
+					_currentPlay = 0.0f;
+				}
+				break;
+		}
+	}
+}
---
+++ Assets/Scripts/WingFlap.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WingFlap : MonoBehaviour {
+
+	public float _maxRotation = Mathf.PI / 4.0f;
+	public float _minRotation = -Mathf.PI / 4.0f;
+    public float _speed = 1.0f;
+
+    private float _angle = 0.0f;
+	private float _direction = 1.0f;
+	private Transform _transform;
+
+	// Use this for initialization
+	void Start () {
+		_transform = GetComponent<Transform>() as Transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _angle += _speed * _direction * Time.deltaTime;
+        if (_angle >= _maxRotation || _angle <= _minRotation)
+        {
+            _direction = -_direction;
+        }
+        _angle = Mathf.Clamp(_angle, _minRotation, _maxRotation);
+        Debug.Log(_angle);
+        _transform.Rotate(Vector3.forward, _angle);
+	}
+}
